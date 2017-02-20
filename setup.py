1bc1ecffa98edd01c9bbcb5dfdd8f1c8d0ecd407
Colum McGaley <cxm7688@rit.edu>
2017-02-20 11:10:43 -0500

    Added py2exe to setup.py file for building win32 executables

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import py2exe
+
+from glob import glob
+
 config = {
     'description': 'A Python client for the Microsoft AD Certificate Services web page',
     'author': 'Magnus Watn',
-    'url': 'https://github.com/magnuswatn/certsrv',
-    'download_url': 'https://github.com/magnuswatn/certsrv',
+    'url': 'https://github.com/volfco/certsrv',
+    'download_url': 'https://github.com/volfco/certsrv',
     'version': '0.9',
     'py_modules': ['certsrv'],
-    'name': 'certsrv'
+    'name': 'certsrv',
+	'console': ['certsrv.py'],
+	'data_files': [("Microsoft.VC140.CRT", glob(r'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\*.*'))]
 }
 
 setup(**config)

f97b500a7af4e479b4e0b70dfb9ffb301e383787
Colum McGaley <cxm7688@rit.edu>
2017-02-20 11:16:21 -0500

    Incremented Version number

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ from glob import glob
 
 config = {
     'description': 'A Python client for the Microsoft AD Certificate Services web page',
-    'author': 'Magnus Watn',
+    'author': 'Colum McGaley',
     'url': 'https://github.com/volfco/certsrv',
     'download_url': 'https://github.com/volfco/certsrv',
-    'version': '0.9',
+    'version': '2.0.3',
     'py_modules': ['certsrv'],
     'name': 'certsrv',
 	'console': ['certsrv.py'],

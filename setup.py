4eaebc5d69a7ea0a13fb63a13a966741948f62bf
Colum McGaley <cxm7688@rit.edu>
2017-02-20 15:17:11 -0500

    Removed py2exe

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@ try:
 except ImportError:
     from distutils.core import setup
 
-import py2exe
-
-from glob import glob
-
 config = {
     'description': 'A Python client for the Microsoft AD Certificate Services web page',
     'author': 'Colum McGaley',
@@ -16,7 +12,6 @@ config = {
     'py_modules': ['certsrv'],
     'name': 'certsrv',
 	'console': ['certsrv.py'],
-	'data_files': [("Microsoft.VC140.CRT", glob(r'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\*.*'))]
 }
 
 setup(**config)
